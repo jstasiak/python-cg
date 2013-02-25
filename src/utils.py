ed0551007c2f7bac2639aad8ced0b084d26efcb4
Jakub Stasiak <jakub@stasiak.at>
2013-02-25 21:45:00 +0000

    Fix minor bug for Python 2.x

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from abc import ABCMeta, abstractmethod
 
-WithABCMeta = ABCMeta('WithABCMeta', (object,), {})
+WithABCMeta = ABCMeta(str('WithABCMeta'), (object,), {})
 
 class Disposable(WithABCMeta):
 	'''
