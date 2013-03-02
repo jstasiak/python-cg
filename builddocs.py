82185d54542711d40c651176a6f930913c7c9098
Jakub Stasiak <jakub@stasiak.at>
2013-03-02 23:20:00 +0000

    Update documentation building script

--- builddocs.py
+++ builddocs.py
@@ -10,6 +10,8 @@ def main():
 	check_call([sys.executable, 'setup.py', 'build_ext', '--inplace'])
 	sphinx_build = join(dirname(sys.executable), 'sphinx-build')
 	check_call([sphinx_build, '-b', 'html', 'docs', join('gh-pages', 'html')])
+	with open(join('gh-pages', 'html', '.nojekyll'), 'w'):
+		pass
 
 if __name__ == '__main__':
 	main()
