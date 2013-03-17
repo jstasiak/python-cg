81a2938e0e647185e87f127c99dccc78aaf47be2
Jakub Stasiak <jakub@stasiak.at>
2013-03-17 12:44:08 +0000

    Run tests with coverage enabled

--- runtests.py
+++ runtests.py
@@ -8,6 +8,9 @@ from subprocess import check_call
 
 def main():
 	check_call([sys.executable, 'setup.py', 'build_ext', '--inplace'])
+	if '--with-coverage' not in sys.argv:
+		sys.argv.extend(('--with-coverage', '--cover-package=cg'))
+
 	sys.exit(
 		load_entry_point('nose', 'console_scripts', 'nosetests')()
 	)
